6abf271c13def2163cd75e366f8b142530c99076
diana263-ai <dianarudo87@gmail.com>
2023-01-24 11:06:35 +0200

    initial commit using laravel 9, PHP 8

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+  
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BlogController;
+  
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+  
+Route::post('login', [AuthController::class, 'signin']);
+Route::post('register', [AuthController::class, 'signup']);
+     
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('blogs', BlogController::class);
+});
+
+//Sanctum provides authentication for Single page applications, mobile applications and simple token based APIs.
+
